c1f08e7785f1f859d5dfeea504d88b12811e9fe2
Chris Berger <cberger4@users.noreply.github.com>
2020-07-28 13:24:39 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,113 @@
+let song;
+let toggle, nosleep;
+let bands16, bands64, bands256, bands1024;
+let fft;
+let numBands = 64; //must be power of 2 >= 16
+let w;
+let smoothing = 0.9;
+let offset = 50; //start point of visualizer
+let buffer = 50; //space from top of canvas
+let loadSong = 1;
+
+function preload() {
+  song1 = loadSound('sounds/this-groove.mp3');
+  song2 = loadSound('sounds/no-sleep.mp3');
+
+  song = song1;
+
+}
+
+function setup() {
+  createCanvas(512, 512);
+
+  toggle = createButton('Play/Pause');
+  toggle.position(50, height - 90);
+  toggle.mousePressed(toggleSong);
+
+  nosleep = createButton('Load No Sleep');
+  nosleep.position(40, height - 60);
+  nosleep.mousePressed(playNoSleep);
+
+  thisgroove = createButton('Load This Groove');
+  thisgroove.position(30, height - 30);
+  thisgroove.mousePressed(playThisGroove);
+
+  bands16 = createButton('16 Bands');
+  bands16.position(200, height - 75);
+  bands16.mousePressed(setBands16);
+
+  bands64 = createButton('64 Bands');
+  bands64.position(200, height - 50);
+  bands64.mousePressed(setBands64);
+
+  bands256 = createButton('256 Bands');
+  bands256.position(300, height - 75);
+  bands256.mousePressed(setBands256);
+
+  bands1024 = createButton('1024 Bands');
+  bands1024.position(300, height - 50);
+  bands1024.mousePressed(setBands1024);
+
+  fft = new p5.FFT(smoothing, numBands);
+  w = width / numBands;
+}
+
+function draw() {
+  background(0);
+  let spectrum = fft.analyze();
+  stroke(255);
+  noFill();
+  //console.log(spectrum);
+
+  for (i = 0; i < spectrum.length; i++) {
+    let amp = spectrum[i];
+    let y = map(amp, 0, 255, height - offset, offset + buffer);
+    rect(i * w, y - offset, w, height - y - offset, w);
+  }
+
+}
+
+function toggleSong() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else song.play();
+}
+
+function playNoSleep() {
+  if (song.isPlaying()) {
+    song.stop();
+  }
+  song = song2;
+}
+
+function playThisGroove() {
+  if (song.isPlaying()) {
+    song.stop();
+  }
+  song = song1;
+}
+
+
+function setBands16() {
+  numBands = 16;
+  fft = new p5.FFT(smoothing, numBands);
+  w = width / numBands;
+}
+
+function setBands64() {
+  numBands = 64;
+  fft = new p5.FFT(smoothing, numBands);
+  w = width / numBands;
+}
+
+function setBands256() {
+  numBands = 256;
+  fft = new p5.FFT(smoothing, numBands);
+  w = width / numBands;
+}
+
+function setBands1024() {
+  numBands = 1024;
+  fft = new p5.FFT(smoothing, numBands);
+  w = width / numBands;
+}
